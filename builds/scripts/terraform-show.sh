e02331b6bf01c88347c13e0bddee42c561b8a088
peabnuts123 <peabnuts123@gmail.com>
2020-10-13 19:29:00 +1300

    [META] Add build/deploy scripts for common tasks

---
+++ builds/scripts/terraform-show.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e;
+
+function exit_with_message_and_code() {
+  code="$1";
+  message="$2";
+  echo "Error: ${message}";
+  exit "$code";
+}
+
+cd terraform || exit_with_message_and_code 1 "Must run $0 from project root.";
+
+terraform show | tail -n 5;
