be911bb25dece714b080c625fd6a10e2894f0515
peabnuts123 <peabnuts123@gmail.com>
2020-10-20 15:28:26 +1300

    [API] Remove un-needed script

--- src/api/PetGame.Web/deploy-lambda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e;
-
-rm -rf bin/Release;
-dotnet publish --configuration Release;
-cd bin/Release/netcoreapp3.1/publish || exit 1;
-zip -r api.zip .;
-aws lambda update-function-code --function-name pet-game-api --zip-file fileb://api.zip --profile 'petgame';
