7b8c15b5aa5cf5aa6af6fa8742308d2159573660
peabnuts123 <peabnuts123@gmail.com>
2020-11-03 17:56:38 +1300

    [WWW] Add bappy-flirb game to games page, with responsive layout

--- src/www/src/routes/games.tsx
+++ src/www/src/routes/games.tsx
@@ -1,22 +1,44 @@
 import { observer } from 'mobx-react-lite';
 import { Fragment, FunctionalComponent, h } from 'preact';
+import { useEffect, useRef } from 'preact/hooks';
+import { getCurrentUrl } from 'preact-router';
+
+import Logger, { LogLevel } from '@app/util/Logger';
 
 const GamesRoute = observer(() => {
+  const aspectRatioMaintainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize);
+    onResize();
+
+    const RESIZE_DEBOUNCE_TIME_MS = 30;
+    let onResizeTimer: number | undefined = undefined;
+
+    return () => window.removeEventListener('resize', onResize);
+
+    function onResize(): void {
+      clearTimeout(onResizeTimer);
+      onResizeTimer = window.setTimeout(() => {
+        // Ensure we don't do this after user has navigated
+        if (getCurrentUrl().startsWith('/games') && aspectRatioMaintainerRef.current) {
+          Logger.log(LogLevel.debug, "Game route is resizing", getCurrentUrl());
+          const aspectRatioMaintainerEl = aspectRatioMaintainerRef.current;
+          aspectRatioMaintainerEl.style['paddingBottom'] = `${100 * (window.innerHeight / window.innerWidth)}%`;
+        }
+      }, RESIZE_DEBOUNCE_TIME_MS);
+    }
+  }, []);
+
   return (
     <Fragment>
       <h1>Games!</h1>
-      <ul>
-        <li>
-          <em><small>tooooons!</small></em>
-        </li>
-        <li>
-          <em><small>chawacters</small></em>
-        </li>
-        <li>
-          <em><small>h-emaaail</small></em>
-        </li>
-      </ul>
 
+      <div class="games__aspect-maintainer" ref={aspectRatioMaintainerRef}>
+        <div class="games__container">
+          <iframe src="https://pet-game-dev-game-bappy-flirb.s3.ap-southeast-2.amazonaws.com/index.html" frameBorder="0" class="games__iframe"></iframe>
+        </div>
+      </div>
     </Fragment>
   );
 }) as FunctionalComponent;
