0cac8ff1532994eaf96f0d1f9fdc54f92e9b9f90
peabnuts123 <peabnuts123@gmail.com>
2020-11-22 11:08:30 +1300

    [WWW] Add leaderboard to games page

--- src/www/src/routes/games.tsx
+++ src/www/src/routes/games.tsx
@@ -1,16 +1,42 @@
 import { observer } from 'mobx-react-lite';
 import { Fragment, FunctionalComponent, h } from 'preact';
-import { useEffect, useRef } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import { getCurrentUrl } from 'preact-router';
 
 import Logger, { LogLevel } from '@app/util/Logger';
+import LeaderboardEntry from '@app/models/LeaderboardEntry';
+import { useStores } from '@app/stores';
+import LoadingSpinner from '@app/components/loading-spinner';
+
+/** Time period to debounce resize function calls */
+const RESIZE_DEBOUNCE_TIME_MS = 30;
+
+/** Unique GUID for Bappy Flirb game - matches API / DB */
+const BAPPY_FLIRB_GAME_ID: string = 'bf06df8d-276f-40d2-975a-f57f2042d8c2';
+
+const NUM_TOP_SCORES: number = 20;
 
 const GamesRoute = observer(() => {
+  const { LeaderboardStore } = useStores();
+
   const aspectRatioMaintainerRef = useRef<HTMLDivElement | null>(null);
+  const [isLoadingTopScores, setIsLoadingTopScores] = useState<boolean>(true);
+  const [topScores, setTopScores] = useState<LeaderboardEntry[]>([]);
 
   useEffect(() => {
-    /** Time period to debounce resize function calls */
-    const RESIZE_DEBOUNCE_TIME_MS = 30;
+    // Fetch scores from API
+    void LeaderboardStore.GetTopEntriesForGame(BAPPY_FLIRB_GAME_ID, NUM_TOP_SCORES)
+      .then((result: LeaderboardEntry[]) => {
+        setTopScores(result);
+        setIsLoadingTopScores(false);
+      })
+      .catch((error) => {
+        setIsLoadingTopScores(false);
+        Logger.log(LogLevel.debug, `Failed to fetch top scores: `, error);
+      });
+
+
+    // Window resize logic for iframe
     /** Timer handle to last setTimeout */
     let onResizeTimer: number | undefined = undefined;
 
@@ -31,15 +57,13 @@ const GamesRoute = observer(() => {
       }, RESIZE_DEBOUNCE_TIME_MS);
     };
 
-    Logger.log(LogLevel.debug, "Subscring to resize listener");
-
     // Subscribe to 'resize' event (and invoke immediately)
     window.addEventListener('resize', onResize);
     onResize();
 
     // Remove listener on leaving page
     return () => window.removeEventListener('resize', onResize);
-  }, []);
+  }, [LeaderboardStore]);
 
   return (
     <Fragment>
@@ -53,7 +77,36 @@ const GamesRoute = observer(() => {
       </div>
 
       <h2>Leaderboard</h2>
-      <p>Coming soon!</p>
+      <div class="games__leaderboard">
+        {isLoadingTopScores ? (
+          <LoadingSpinner />
+        ) : (
+            (topScores.length === 0 ? (
+              <p class="games__leaderboard__no-scores-message">Nobody has submitted a score for this game. Be the first!</p>
+            ) : (
+                <table class="games__leaderboard__table">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Score</th>
+                      <th class="games__leaderboard__table__user--th">User</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {topScores.map((score, index) => (
+                      <tr key={score.id}>
+                        <td>{index + 1}</td>
+                        <td>{score.score.toLocaleString()}</td>
+                        <td class="games__leaderboard__table__user--td">{score.user.username}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )
+            )
+          )
+        }
+      </div>
     </Fragment>
   );
 }) as FunctionalComponent;
